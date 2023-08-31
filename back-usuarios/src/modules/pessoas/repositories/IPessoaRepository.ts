d61b1856c942734a816561afbb77b97e8a280b91
Hyuri.Cortes <hyuri.miranda.cortes@gmail.com>
2023-08-31 11:11:25 -0300

    feat: create users

---
+++ back-usuarios/src/modules/pessoas/repositories/IPessoaRepository.ts
@@ -0,0 +1,10 @@
+import { ICreatePessoaDTO } from "../dtos/ICreatePessoaDTO";
+import { Pessoa } from "../entities/Pessoa";
+
+interface IPessoaRepository{
+    create(data: ICreatePessoaDTO): Promise <void>;
+    findById(id:string): Promise <Pessoa>;
+    findByEmail(email:string): Promise <Pessoa>;
+}
+
+export {IPessoaRepository}
---
+++ back-usuarios/src/modules/pessoas/useCases/createUser/CreatePessoaController.ts
@@ -0,0 +1,17 @@
+import { container } from "tsyringe"
+import {Request,Response} from "express";
+import { CreatePessoaUseCase } from "./CreatePessoaUseCase"
+
+class CreatePessoaController{
+   async handle(request: Request, response: Response): Promise <Response>{
+        const {nome, email, dataNascimento} = request.body;
+
+        const createPessoaUseCase = container.resolve(CreatePessoaUseCase);
+    
+        await createPessoaUseCase.execute({nome, email, dataNascimento});
+
+        return response.status(201).send();
+    }
+}
+
+export { CreatePessoaController}
